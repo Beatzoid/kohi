246c1a1f5a41989628cbde08364e0915bcf93ae3
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2022-06-17 17:31:38 -0700

    ✨ Vulkan Swapchain, Images and Depth Buffer

--- engine/src/core/renderer/vulkan/vulkan_backend.c
+++ engine/src/core/renderer/vulkan/vulkan_backend.c
@@ -2,6 +2,7 @@
 #include "vulkan_types.inl"
 #include "vulkan_platform.h"
 #include "vulkan_device.h"
+#include "vulkan_swapchain.h"
 
 #include "core/logger/logger.h"
 #include "core/kstring/kstring.h"
@@ -17,8 +18,12 @@ VKAPI_ATTR VkBool32 VKAPI_CALL vk_debug_callback(
     const VkDebugUtilsMessengerCallbackDataEXT *callback_data,
     void *user_data);
 
+i32 find_memory_index(u32 type_filter, u32 property_flags);
+
 b8 vulkan_renderer_backend_initialize(renderer_backend *backend, const char *application_name, struct platform_state *plat_state)
 {
+    context.find_memory_index = find_memory_index;
+
     // TODO: custom allocator.
     context.allocator = 0;
 
@@ -139,6 +144,12 @@ b8 vulkan_renderer_backend_initialize(renderer_backend *backend, const char *app
         return FALSE;
     }
 
+    vulkan_swapchain_create(
+        &context,
+        context.framebuffer_width,
+        context.framebuffer_height,
+        &context.swapchain);
+
     // Free up some memory (like 5kb lol but whatever)
     darray_destroy(avalible_layers);
     darray_destroy(required_validation_layer_names);
@@ -150,12 +161,18 @@ b8 vulkan_renderer_backend_initialize(renderer_backend *backend, const char *app
 
 void vulkan_renderer_backend_shutdown(renderer_backend *backend)
 {
+    KDEBUG("Destroying swapchain");
+    vulkan_swapchain_destroy(&context, &context.swapchain);
 
     KDEBUG("Destroying Vulkan device...");
     vulkan_device_destroy(&context);
 
     KDEBUG("Destrying Vulkan surface...");
-    vkDestroySurfaceKHR(context.instance, context.surface, context.allocator);
+    if (context.surface)
+    {
+        vkDestroySurfaceKHR(context.instance, context.surface, context.allocator);
+        context.surface = 0;
+    }
 
 #if _DEBUG
     KDEBUG("Destroying Vulkan debugger...");
@@ -206,4 +223,21 @@ VKAPI_ATTR VkBool32 VKAPI_CALL vk_debug_callback(
         break;
     }
     return VK_FALSE;
+}
+
+i32 find_memory_index(u32 type_filter, u32 property_flags)
+{
+    VkPhysicalDeviceMemoryProperties memory_properties;
+    vkGetPhysicalDeviceMemoryProperties(context.device.physical_device, &memory_properties);
+
+    for (u32 i = 0; i < memory_properties.memoryTypeCount; ++i)
+    {
+        if (type_filter & (1 << i) && (memory_properties.memoryTypes[i].propertyFlags & property_flags) == property_flags)
+        {
+            return i;
+        }
+    }
+
+    KWARN("Unable to find suitable memory type!");
+    return -1;
 }
